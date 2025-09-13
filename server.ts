4f728c695060f6e5d714070fc962abe019079a26
Victor Martins <victor2007azevedo@hotmail.com>
2025-09-13 12:48:05 -0300

    fix: Porta correta

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import app from "./src/app";
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
